8fedc610dd905beb756f8e2cfd97922f15702df7
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2024-01-05 23:24:16 +0100

    Laravel init

---
+++ app/Models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'username',
+        'email',
+        'password',
+        'api_key',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'api_key',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'api_key' => 'hashed'
+        ];
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function BankAccounts(): HasMany
+    {
+        return $this->hasMany(related: BankAccount::class, foreignKey: 'user_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function TransactionCategories(): HasMany
+    {
+        return $this->hasMany(related: TransactionCategory::class, foreignKey: 'user_id');
+    }
+}
