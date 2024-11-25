c2b097a94227290a7fb93e7003de23f993467247
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2024-11-25 21:03:25 +0100

    Update test for unverified user

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Filament\Pages\Auth\EditProfile;
+use App\Filament\Pages\Dashboard;
 use App\Models\User;
 
 it('redirects to the login page', function () {
@@ -10,14 +12,22 @@ it('redirects to the login page', function () {
 
 it('displays the login page', function () {
     $response = $this->get('/login');
-
     $response->assertStatus(200);
 });
 
-it('shows the dashboard for an authenticated user', function () {
+it('redirects an unverified user to the profile', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
     $response = $this->get('/');
+
+    $response->assertRedirect(EditProfile::getUrl());
+    $user->delete();
+});
+
+it('shows the dashboard to an verified user', function () {
+    $user = User::factory(['verified' => true])->create();
+    $this->actingAs($user);
+    $response = $this->get(Dashboard::getUrl());
 
     $response->assertStatus(200);
     $user->delete();
