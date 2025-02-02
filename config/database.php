9d5848df2e6be9edf745694ff9794b43c8010001
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2025-02-02 13:22:34 +0100

    Improve dev environment

--- config/database.php
+++ config/database.php
@@ -2,6 +2,32 @@
 
 return [
 
+    'default' => env('DB_CONNECTION', 'mariadb'),
+
+    'connections' => [
+
+        'mariadb' => [
+            'driver' => 'mariadb',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', 'fin-tracker-db'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'fin-tracker'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+    ],
+
     'migrations' => [
         'table' => 'sys_migrations',
     ],
