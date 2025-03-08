01a7d4d0d6c547436c03e9813f496e7cbf4d15b2
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2025-03-08 21:12:15 +0100

    Add command to fix database timezones

--- app/Console/Commands/CleanupDataCommand.php
+++ app/Console/Commands/CleanupDataCommand.php
@@ -30,24 +30,17 @@ use Illuminate\Support\Facades\Storage;
 final class CleanupDataCommand extends Command
 {
     /**
-     * The name and signature of the console command.
-     *
      * @var string
      */
     protected $signature = 'app:cleanup-data';
 
     /**
-     * The console command description.
-     *
      * @var string
      */
     protected $description = 'Cleanup old data.';
 
     private CarbonInterface $cutoff;
 
-    /**
-     * Execute the console command.
-     */
     public function handle(): int
     {
         $this->cutoff = Carbon::now()->subDays(7);
