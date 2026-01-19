8e3d5041fb5267481e1a8fd59040fdddd953ef49
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2026-01-19 21:45:23 +0100

    Add api exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Handlers\ApiExceptionHandler;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,12 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
             __DIR__.'/../routes/auth.php',
             __DIR__.'/../routes/web.php',
         ],
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        new ApiExceptionHandler()->register($exceptions);
     })->create();
